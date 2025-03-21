1560c263767b3736b9b2903121917d2c64d2ea2b
saleemafroz <saeemafroze@gmail.com>
2025-03-21 20:28:33 +0530

    test code

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "google_compute_network" "vpc_network" {
 # 2. Subnet Configuration
 resource "google_compute_subnetwork" "subnet" {
   name          = "subnet-1"
-  region        = "us-west1"  # Updated region
   network       = google_compute_network.vpc_network.name
   ip_cidr_range = "10.0.0.0/24"
 }
@@ -23,7 +22,7 @@ resource "google_compute_instance" "vm_instance" {
   zone         = "us-central1-a"
   boot_disk {
     initialize_params {
-      image = "debian-11-buster-v20210129"
+      image = "debian-11"
     }
   }
 
@@ -43,7 +42,6 @@ resource "google_compute_instance" "vm_instance" {
 # 4. Google Kubernetes Engine (GKE) Cluster Configuration
 resource "google_container_cluster" "aks_cluster" {
   name     = "my-aks-cluster"
-  location = "us-central1-a"
 
   initial_node_count = 1
   node_config {
@@ -60,5 +58,4 @@ resource "google_compute_disk" "disk" {
   name  = "my-disk"
   size  = 10  # Size in GB
   type  = "pd-standard"  # Standard persistent disk
-  zone  = "us-central1-a"
 }
