0b7f856ac675450e4401ad8861629cc17939db73
avinash <singh.avinash943@gmail.com>
2025-07-22 19:22:54 +0530

    my fir terraform-git

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "afroz-terraform"  # GCS bucket name
+    bucket = " my-first-bucket-v1"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
   }
 }
